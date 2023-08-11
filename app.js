b79d06c85b0b77c8c872d3f5daa68fa2bbdc5ed0
spaceKSRW <kannuurawat18@outlook.com>
2023-08-12 00:01:00 +0530

    first project commit git pull

---
+++ app.js
@@ -0,0 +1,17 @@
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const user = require("./routes/main");
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(user);
+
+app.listen(4000, () => {
+  console.log("I am Listening on port 4000");
+  console.log("localhost:4000");
+});
